09adf91840fc20b188b8acb59ef00898d4abf70a
Shruthi Yenamagandla <yshruthi.14@gmail.com>
2024-09-08 22:15:24 -0500

    changed colors

--- app/page.js
+++ app/page.js
@@ -77,7 +77,7 @@ export default function Waitlist() {
         </BackgroundGradientAnimation>
       </section>
 
-      <section id="LearnMore" className="flex w-screen h-screen bg-[#CCB4FF]">    
+      <section id="LearnMore" className="flex w-screen h-screen bg-gradient-to-b from-[#2C174C] to-[#07052D]">    
         <div className="m-24">
           <div className="flex flex-col space-y-10">
             <div className="flex flex-row space-x-48"> 
@@ -88,7 +88,7 @@ export default function Waitlist() {
                   animate={learnMoreInView ? "visible" : "hidden"}
                   variants={fadeZoomInOut}
                   transition={{ delay: 0.2, duration: 0.6 }}
-                  className="text-shadow-custom">Your
+                  className="">Your
                 </motion.div> 
                 
                 <motion.div 
@@ -97,7 +97,7 @@ export default function Waitlist() {
                   animate={learnMoreInView ? "visible" : "hidden"}
                   variants={fadeZoomInOut}
                   transition={{ delay: 0.8, duration: 0.6 }}
-                  className="text-shadow-custom">Ultimate
+                  className="">Ultimate
                 </motion.div>
                 
                 <motion.div 
@@ -106,25 +106,25 @@ export default function Waitlist() {
                   variants={fadeZoomInOut}
                   animate={learnMoreInView ? "visible" : "hidden"}
                   transition={{ delay: 1.4, duration: 0.6 }}
-                  className="text-shadow-custom">Campus Companion
+                  className="">Campus Companion
                 </motion.div>
               </div>
               <div className="font-chillax font-normal text-md text-right leading-7"> 
-                <div className="mb-6 text-[#2C174B] font-normal"> Clarity AI is like that one friend who always <br/> knows what's up. </div>
-                <div className="text-[#2C174B]"> 
+                <div className="mb-6 text-[#E8DBFF] font-normal"> Clarity AI is like that one friend who always <br/> knows what's up. </div>
+                <div className="text-[#E8DBFF]"> 
                   <div className="font-semibold"> Need quick answers? </div> 
                   <div className="mb-6"> We've got you. </div> 
                 </div>
-                <div className="text-[#2C174B]"> 
+                <div className="text-[#E8DBFF]"> 
                   <div className="font-semibold"> Want to connect with classmates? </div> 
                   <div className="mb-6"> We've got that too. </div> 
                 </div>
-                <div className="text-[#2C174B]"> It's like having a cheat code for <br/> university, minus the actual cheating. </div>
+                <div className="text-[#E8DBFF]"> It's like having a cheat code for <br/> university, minus the actual cheating. </div>
               </div>
             </div>
 
             <a href="#ThankYou">
-              <button className="font-chillax font-medium w-44 text-[#E8DBFF] bg-[#64409D] rounded-full px-5 py-1 shadow-lg shadow-[#A781E2] border border-t-0 border-[#544072] cursor-pointer hover:-translate-y-3 duration-300"> Join Clarity AI </button>
+              <button className="font-chillax font-medium w-44 text-[#07052D] bg-[#E8DBFF] rounded-full px-5 py-1 shadow-[#A781E2] border border-t-0 border-[#544072] cursor-pointer hover:-translate-y-3 duration-300"> Join Clarity AI </button>
             </a>
 
             <motion.div 
@@ -155,7 +155,7 @@ export default function Waitlist() {
       </section>
 
 
-      <section id="ThankYou" className="flex w-screen h-screen bg-[#CCB4FF]">
+      <section id="ThankYou" className="flex w-screen h-screen bg-gradient-to-b from-[#07052D] to-[#2C174C]">
   
         <div className="flex flex-col space-y-24 m-24">
           <motion.div 
@@ -165,15 +165,16 @@ export default function Waitlist() {
             variants={slideFromLeft}
             transition={{ duration: 1.2 }}
             className="flex flex-col space-y-10">
-            <div className="font-axiforma font-bold text-[#E8DBFF] text-7xl text-shadow-custom"> Thank you </div>    
-            <div className="font-axiforma font-bold text-[#E8DBFF] text-7xl text-shadow-custom"> for your interest in Clarity AI! </div>
+            <div className="font-axiforma font-bold text-[#E8DBFF] text-7xl"> Thank you </div>    
+            <div className="font-axiforma font-bold text-[#E8DBFF] text-7xl"> for your interest in Clarity AI! </div>
           </motion.div>
 
           <div className="flex flex-row space-x-44"> 
-            <div className="flex flex-col space-y-8 text-[#2C174B] font-chillax font-normal leading-6">
-              <div> To be considered for early access, we kindly ask that you follow us on our social media accounts. <br/> As a <span className="font-semibold"> non-profit organization, </span> every follow helps us grow our community and continue providing <br/> valuable resources for students like you. 
+            <div className="flex flex-col space-y-8 text-[#E8DBFF] font-chillax font-normal leading-32">
+              <div> Take a moment to follow us on <span className="font-semibold"> Instagram, LinkedIn, & Discord </span>. These platforms are where we share <br/> exciting updates and helpful information. </div>
+
+              <div> As a <span className="font-semibold"> non-profit organization, </span> every follow helps us grow our community and continue providing <br/> valuable resources for students like you. 
                     By joining our online community, you'll stay <br/> updated with the latest news, features, and developments about Clarity AI. </div>
-              <div> Take a moment to follow us on Instagram, LinkedIn, & Discord. These platforms are where we share <br/> exciting updates and helpful information. </div>
               <div> Once you've followed us, navigate back to this page to unlock early access and start using our app. </div>
               <div> We appreciate your support! </div>
             </div>
@@ -184,10 +185,10 @@ export default function Waitlist() {
               variants={fadeZoomInOut}
               animate={thankYouInView ? "visible" : "hidden"}
               transition={{ delay: 1.2, duration: 0.6 }}
-              className="flex flex-col space-y-10 text-[#2C174B] font-chillax font-normal">
+              className="flex flex-col space-y-10 text-[#E8DBFF] font-chillax font-normal">
 
               <div className="flex flex-col space-y-4 justify-center items-center">
-                <div className="font-semibold text-xl text-center"> Step 1:  Follow Us </div>
+                <div className="font-semibold text-2xl text-center"> Step 1:  Follow Us </div>
                 <div className="font-normal"> Follow us on our socials, links below! </div>
                 <motion.div 
                   ref={thankYouRef}
@@ -207,12 +208,12 @@ export default function Waitlist() {
                 </motion.div>
               </div>
               <div className="flex flex-col space-y-4 justify-center items-center">
-                <div className="font-semibold text-xl text-center"> Step 2:  Verify </div>
+                <div className="font-semibold text-2xl text-center"> Step 2:  Verify </div>
                 <div className="font-normal"> Have you followed Clarity AI on our socials? </div>
-                <Link href="/waitlistPage" className="font-chillax font-medium w-44 text-center text-[#E8DBFF] bg-[#64409D] rounded-full px-5 py-1 shadow-lg shadow-[#A781E2] border border-t-0 border-[#544072] cursor-pointer hover:-translate-y-2 duration-300"> Yes, Join Now! </Link>
+                <Link href="/waitlistPage" className="font-chillax font-medium w-44 text-center text-[#07052D] bg-[#E8DBFF] rounded-full px-5 py-1 border border-t-0 border-[#544072] cursor-pointer hover:-translate-y-2 duration-300"> Yes, Join Now! </Link>
               </div>
             </motion.div>
-
+            
           </div>
         </div>
 
